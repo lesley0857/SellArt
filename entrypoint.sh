7b30872b25290f5b7892aeaaf6e3ccfa2c5fb2cf
lesley <nwekelesley@gmail.com>
2024-08-08 16:09:58 +0100

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 python manage.py makemigrations
-python manage.py migrate 
-python manage.py collectstatic 
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 # daphne eshopperproject.asgi:application -b 0.0.0.0 -p 8000
 
