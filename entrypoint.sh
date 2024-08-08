100533f9117e1e78971c5efc4108497d6a1971ac
lesley <nwekelesley@gmail.com>
2024-08-08 16:07:26 +0100

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 python manage.py makemigrations
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python manage.py migrate 
+python manage.py collectstatic 
 
-daphne eshopperproject.asgi:application -b 0.0.0.0 -p 8000
+# daphne eshopperproject.asgi:application -b 0.0.0.0 -p 8000
 
 celery --app=eshopperproject worker --pool=solo -l INFO
 celery -A eshopperproject beat -l INFO
