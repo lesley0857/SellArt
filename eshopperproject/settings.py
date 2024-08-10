e3697a6807cb8cbef43a66f22fc71e4b456ecaaf
lesley <nwekelesley@gmail.com>
2024-08-10 21:43:02 +0100

    ready with error

--- eshopperproject/settings.py
+++ eshopperproject/settings.py
@@ -158,8 +158,8 @@ DATABASES = {
 #     }
 # }
 
-database_url = os.getenv('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse(database_url)
+# database_url = os.getenv('DATABASE_URL')
+# DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
