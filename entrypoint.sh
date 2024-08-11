a6349b9d6efe5320962994b458f80bf2107cd537
lesley <nwekelesley@gmail.com>
2024-08-11 23:30:23 +0100

    ready with error

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-daphne eshopperproject.asgi:application -b 0.0.0.0 -p 8000
+celery --app=eshopperproject worker --pool=solo -l INFO
+# daphne eshopperproject.asgi:application -b 0.0.0.0 -p 8000
 
