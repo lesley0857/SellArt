e022785f32f3bc43e9b23e8f57c00e186cd50a3f
lesley <nwekelesley@gmail.com>
2024-09-03 16:59:53 +0100

    trying s3 again

--- eshopperproject/settings.py
+++ eshopperproject/settings.py
@@ -124,23 +124,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv('NAME'),
-#         "USER": os.getenv('USER'),
-#         "PASSWORD": os.getenv('PASSWORD'),
-#         "HOST": os.getenv('HOST'),
-#         "PORT": 5432,
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('NAME'),
+        "USER": os.getenv('USER'),
+        "PASSWORD": os.getenv('PASSWORD'),
+        "HOST": os.getenv('HOST'),
+        "PORT": 5432,
+    }
+}
 
 # database_url = ""
 # # os.getenv('DATABASE_URL')
