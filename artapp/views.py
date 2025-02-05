8ca8b812f5f526b2237e4891daa4d520db5fb3f7
lesley <nwekelesley@gmail.com>
2025-02-05 22:49:37 +0000

    removed_categoryTab_also_added_footer

--- artapp/views.py
+++ artapp/views.py
@@ -5,8 +5,10 @@ from .models import *
 
 def product_view(request, id):
     cat = Category.objects.filter(name=id).first()
+    category = Category.objects.all()
     slider1 = Artproduct.objects.filter(category=cat).first()
     art = Artproduct.objects.all()
     products = Artproduct.objects.filter(name=id).first()
-    context = {'slider1': slider1, 'products': products, 'art': art}
+    context = {'category': category, 'slider1': slider1,
+               'products': products, 'art': art}
     return render(request, 'products.html', context)
