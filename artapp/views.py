4f9dc902774861ac3a127a43c3677d4024cded2d
lesley <nwekelesley@gmail.com>
2025-02-10 01:24:09 +0000

    other_pages_addes

--- artapp/views.py
+++ artapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 
@@ -8,7 +10,18 @@ def product_view(request, id):
     category = Category.objects.all()
     slider1 = Artproduct.objects.filter(category=cat).first()
     art = Artproduct.objects.all()
-    products = Artproduct.objects.filter(name=id).first()
+    products = Artproduct.objects.filter(name=id)
+    category = Category.objects.all()
+    product_to_display = Paginator(products, 4)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = product_to_display.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = product_to_display.page(1)
+    except EmptyPage:
+        page_obj = product_to_display.page(product_to_display.num_pages)
+
     context = {'category': category, 'slider1': slider1,
+               'page_obj': page_obj,
                'products': products, 'art': art}
     return render(request, 'products.html', context)
